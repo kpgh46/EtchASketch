65331017080eb2b520752d85a9ce558d5ea65122
Kevin McPeak <kevmc46@gmail.com>
2021-09-06 19:47:29 -0400

    added mouse over

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const container = document.querySelector('#maincontainer');
 
+
 function addSquares(num){
     for(i = 0; i < (num * num); i++){
         let newSquare = document.createElement("div");
         newSquare.classList.add("squares");
         container.appendChild(newSquare);
-        addEventListener("mouseover",mouseOver);
+        
     }
-    
-}
-
-function mouseOver(){
-    container.style.color = "black";
 }
 
 addSquares(16);
+
+container.addEventListener("mouseover", function(event) {
+    event.target.style.color = "red";
+});
 
 
 
