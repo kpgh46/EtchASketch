f613b8bbeeef47afd1ce4db1ba178f7fb48c69e6
Kevin McPeak <kevmc46@gmail.com>
2021-09-08 12:20:14 -0400

    user input updates grid

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('#maincontainer');
+const button = document.querySelector('#button');
 
 
 function addSquares(num){
@@ -16,5 +17,16 @@ container.addEventListener("mouseover", function(event) {
     event.target.style.backgroundColor = "red";
 });
 
+button.addEventListener('click', () => {
+    let askUser = prompt ("How many blocks?")
+    askUser = parseInt(askUser);
 
+    container.style.setProperty("--column-row", askUser);
+
+    for(i = 0; i < (askUser * askUser); i++){
+        let newSquare = document.createElement("div");
+        newSquare.classList.add("squares");
+        container.appendChild(newSquare);
+
+}});
 
