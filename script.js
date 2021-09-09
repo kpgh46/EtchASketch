fe67ecab1fb6560951a9c2841d2baf76f1ee4660
Kevin McPeak <kevmc46@gmail.com>
2021-09-09 09:27:42 -0400

    added functionality to clear maincontainer when button is pushed

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.querySelector('#maincontainer');
 const button = document.querySelector('#button');
 
 
+
 function addSquares(num){
     for(i = 0; i < (num * num); i++){
         let newSquare = document.createElement("div");
@@ -11,7 +12,7 @@ function addSquares(num){
     }
 }
 
-addSquares(16);
+
 
 container.addEventListener("mouseover", function(event) {
     event.target.style.backgroundColor = "red";
@@ -21,12 +22,13 @@ button.addEventListener('click', () => {
     let askUser = prompt ("How many blocks?")
     askUser = parseInt(askUser);
 
+    document.getElementById("maincontainer").innerHTML = "";
+
     container.style.setProperty("--column-row", askUser);
 
     for(i = 0; i < (askUser * askUser); i++){
         let newSquare = document.createElement("div");
         newSquare.classList.add("squares");
         container.appendChild(newSquare);
-
 }});
 
